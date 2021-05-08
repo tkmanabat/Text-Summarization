2ee5552b9c43cb197ad6e0470c77b3c406ec14bf
tkmanabat <tkmanabat@gmail.com>
2021-05-08 16:43:27 +0800

    added st forms

--- app.py
+++ app.py
@@ -18,14 +18,18 @@ st.write("""
 Using Hugging Face Transformers 🤗
 """)
 
-input=st.text_area('Enter your Text',height=300)
+with st.form(key='my_form'):
 
-left_column, right_column=st.beta_columns(2)
+    input=st.text_area('Enter your Text',height=300)
 
-min=left_column.number_input('Minimum words',value=30)
-max=right_column.number_input('Maximum words',value=130)
+    left_column, right_column=st.beta_columns(2)
 
-summarize=st.button('Summarize!')
+    min=left_column.number_input('Minimum words',value=30)
+    max=right_column.number_input('Maximum words',value=130)
+
+    summarize=st.form_submit_button('Summarize!')
+
+
 if summarize:
     summary=summarizer(input,max_length=max, min_length=min, do_sample=False)
     st.subheader('Result 🎉')
